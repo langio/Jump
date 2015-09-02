6dabdbb32b2842153214ceb2f67c30ee45051a16
langio <langio@github.com>
2015-09-02 17:08:59 +0800

    makefile

---
+++ server/3rd/libxl-3.6.2.0/libxl.mk
@@ -0,0 +1,13 @@
+LIBXL_DIR := $(shell dirname `echo $(MAKEFILE_LIST) | awk '{for(i=1;i<=NF;++i){if(\$$i~"libxl.mk") {print \$$i;exit}}}'`)
+
+
+ARCH = $(shell getconf LONG_BIT)
+ifeq ($(ARCH), 32)
+  LIBPATH = $(LIBXL_DIR)/lib
+else
+  LIBPATH = $(LIBXL_DIR)/lib64
+endif
+
+MYCFLAGS = -L $(LIBPATH) -lxl -Wl,-rpath,$(LIBPATH)
+
+INCLUDE += -I$(LIBXL_DIR)/include_cpp
