918e6a40089b0bb5c9cfcea8db5c9080a3263958
langio <langio@github.com>
2015-09-09 19:51:18 +0800

    xc

--- server/3rd/tc/tc.mk
+++
@@ -1,8 +0,0 @@
-TC_DIR := $(shell dirname `echo $(MAKEFILE_LIST) | awk '{for(i=1;i<=NF;++i){if(\$$i~"tc.mk") {print \$$i;exit}}}'`)
-
-INCLUDE += -I$(TC_DIR)/include
-LIBS += -L$(TC_DIR)/lib -ltcutil
-
-DEP_MAKE_SUBDIRS += $(TC_DIR)
-TARGET_DEP_OBJS += $(TC_DIR)/lib/libtcutil.a
-
