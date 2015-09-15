7f35cdbe00dbc3b6126997a2eae1916eaa394a35
langio <langio@github.com>
2015-09-15 16:20:50 +0800

    mk

--- server/comm/comm.mk
+++ server/comm/comm.mk
@@ -1,3 +1,5 @@
 COMM_DIR := $(shell dirname `echo $(MAKEFILE_LIST) | awk '{for(i=1;i<=NF;++i){if(\$$i~"comm.mk") {print \$$i;exit}}}'`)
 
-INCLUDE += -I$(COMM_DIR)
+INCLUDE += -I$(COMM_DIR) -I$(COMM_DIR)/table
+
+LIBS += -L$(COMM_DIR)/lib -lcomm
