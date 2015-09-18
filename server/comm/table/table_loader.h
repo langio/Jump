7aae290c9bd3876a893c0035eb78921636519142
langio <langio@github.com>
2015-09-18 11:45:12 +0800

    tb

--- server/comm/table/table_loader.h
+++ server/comm/table/table_loader.h
@@ -15,22 +15,25 @@ using namespace google::protobuf;
 using namespace util;
 
 
-//protobuf的数据类型
-#define DOUBLE "double"			//													对应于c++ double
+//protobuf的数据类型，excel中只支持下面几种，另外支持vector、map，最多嵌套两层
+//#define DOUBLE "double"			//													对应于c++ double
 #define FLOAT "float"			//													对应于c++ float
 #define INT32 "int32"			//使用可变长编码. 对于负数比较低效，如果负数较多，请使用sint32		对应于c++ int32
 #define INT64 "int64"			//使用可变长编码. 对于负数比较低效，如果负数较多，请使用sint64		对应于c++ int64
 #define UINT32 "uint32"			//使用可变长编码											对应于c++ uint32
 #define UINT64 "uint64"			//使用可变长编码											对应于c++ uint64
-#define SINT32 "sint32"			//使用可变长编码. Signed int value. 编码负数比int32更高效		对应于c++ int32
-#define SINT64 "sint64"			//使用可变长编码. Signed int value. 编码负数比int64更高效		对应于c++ int64
-#define FIXED32 "fixed32"		//恒定四个字节。如果数值几乎总是大于2的28次方，该类型比unit32更高效	对应于c++ uint32
-#define FIXED64 "fixed64"		//恒定四个字节。如果数值几乎总是大于2的56次方，该类型比unit64更高效	对应于c++ uint64
-#define SFIXED32 "sfixed32"		//恒定四个字节											对应于c++ int32
-#define SFIXED64 "sfixed64"		//恒定八个字节											对应于c++ int64
+//#define SINT32 "sint32"			//使用可变长编码. Signed int value. 编码负数比int32更高效		对应于c++ int32
+//#define SINT64 "sint64"			//使用可变长编码. Signed int value. 编码负数比int64更高效		对应于c++ int64
+//#define FIXED32 "fixed32"		//恒定四个字节。如果数值几乎总是大于2的28次方，该类型比unit32更高效	对应于c++ uint32
+//#define FIXED64 "fixed64"		//恒定四个字节。如果数值几乎总是大于2的56次方，该类型比unit64更高效	对应于c++ uint64
+//#define SFIXED32 "sfixed32"		//恒定四个字节											对应于c++ int32
+//#define SFIXED64 "sfixed64"		//恒定八个字节											对应于c++ int64
 #define BOOL "bool"				//													对应于c++ bool
-#define STRING "string"			//A string must always contain UTF-8 encoded or 7-bit ASCII text	对应于c++ string
-#define BYTES "bytes"			//包含任意数量顺序的字节									对应于c++ string
+//#define STRING "string"			//A string must always contain UTF-8 encoded or 7-bit ASCII text	对应于c++ string
+//#define BYTES "bytes"			//包含任意数量顺序的字节									对应于c++ string
+
+#define VECTOR "vector"
+#define MAP "map"
 
 
 //Message使用完之后需要delete
@@ -76,20 +79,23 @@ template<typename key, typename value>
 bool CommLoad<key, value>::load2Map(const string& conf_file, int32_t index, map<key, value>& mOut, getKeyFunc getKey)
 {
 
+	bool bRet = true;
 	Book* book = xlCreateBook();
+	Message *msg = NULL;
 
 	__BEGIN_PROC__
 
 	if(NULL == book)
 	{
 		CFG_LOG_ERROR("create xlCreateBook failed!");
-		false;
+		bRet = false;
+		break;
 	}
 
 	if(book->load(conf_file.c_str()))
 	{
 		CFG_LOG_ERROR("load file %s failed!", conf_file.c_str());
-
+		bRet = false;
 		break;
 	}
 
@@ -98,6 +104,7 @@ bool CommLoad<key, value>::load2Map(const string& conf_file, int32_t index, map<
 	if(NULL == sheet)
 	{
 		CFG_LOG_ERROR("getSheet failed, index:%d", index);
+		bRet = false;
 		break;
 	}
 
@@ -109,13 +116,26 @@ bool CommLoad<key, value>::load2Map(const string& conf_file, int32_t index, map<
 	if(NULL == message)
 	{
 		CFG_LOG_ERROR("get message name failed!");
+		bRet = false;
 		break;
 	}
 
 	const string message_name("protocol." + string(message));
 
+	static map<string, int32_t> simple_type;
+	simple_type[FLOAT] = 1;
+	simple_type[INT32] = 1;
+	simple_type[INT64] = 1;
+	simple_type[UINT32] = 1;
+	simple_type[UINT64] = 1;
+	simple_type[BOOL] = 1;
+
+	static map<string, int32_t> complex_type;
+	complex_type[VECTOR] = 1;
+	complex_type[MAP] = 1;
+
 	//从（2,1）开始，处理这一行的所有字段，解析字段中标明的message的字段类型和名字
-	map<string, string> fields;		//map<字段名，类型>
+	map<string, int32_t> fields;		//map<字段名，列号>
 	for(int32_t col=1; col<col_num; ++col)
 	{
 		const char* f = sheet->readStr(2, col);
@@ -125,14 +145,96 @@ bool CommLoad<key, value>::load2Map(const string& conf_file, int32_t index, map<
 			break;
 		}
 
-		//字段的类型名和字段名是使用空格分割的
-		vector<string> v = YAC_Common::sepstr<string>(string(f), " ");
-		if(v.size() != 2)
-		{
-			CFG_LOG_ERROR("invalid unit content:%s", f);
-		}
+		fields[string(f)] = col;
+	}
 
-		fields[v[1]] = v[0];
+
+	//创建message
+	msg = createMessage(message_name);
+	if (NULL == msg)
+	{
+		// 创建失败，可能是消息名错误，也可能是编译后message解析器
+		// 没有链接到主程序中。
+		CFG_LOG_ERROR("createMessage failed");
+		bRet = false;
+		break;
+	}
+
+	// 获取message的descriptor
+	const Descriptor* descriptor = msg->GetDescriptor();
+	// 获取message的反射接口，可用于获取和修改字段的值
+	const Reflection* reflection = msg->GetReflection();
+
+	//从第4行(row=3)开始，正式读取数据
+	for(int32_t row=3; row<row_num; ++row)
+	{
+		for(int32_t i=0; i<descriptor->field_count(); ++i)
+		{
+			const FieldDescriptor* field_descriptor = descriptor->field(i);
+			assert(NULL != field_descriptor);
+
+			const string& strFieldName = field_descriptor->name();
+
+//			enum CppType {
+//			    CPPTYPE_INT32       = 1,     // TYPE_INT32, TYPE_SINT32, TYPE_SFIXED32
+//			    CPPTYPE_INT64       = 2,     // TYPE_INT64, TYPE_SINT64, TYPE_SFIXED64
+//			    CPPTYPE_UINT32      = 3,     // TYPE_UINT32, TYPE_FIXED32
+//			    CPPTYPE_UINT64      = 4,     // TYPE_UINT64, TYPE_FIXED64
+//			    CPPTYPE_DOUBLE      = 5,     // TYPE_DOUBLE
+//			    CPPTYPE_FLOAT       = 6,     // TYPE_FLOAT
+//			    CPPTYPE_BOOL        = 7,     // TYPE_BOOL
+//			    CPPTYPE_ENUM        = 8,     // TYPE_ENUM
+//			    CPPTYPE_STRING      = 9,     // TYPE_STRING, TYPE_BYTES
+//			    CPPTYPE_MESSAGE     = 10,    // TYPE_MESSAGE, TYPE_GROUP
+//
+//			    MAX_CPPTYPE         = 10,    // Constant useful for defining lookup tables
+//			                                 // indexed by CppType.
+//			  };
+			switch(field_descriptor->cpp_type())
+			{
+
+				case FieldDescriptor::CPPTYPE_INT32:
+				{
+					break;
+				}
+				case FieldDescriptor::CPPTYPE_UINT32:
+				{
+					break;
+				}
+				case FieldDescriptor::CPPTYPE_INT64:
+				{
+					break;
+				}
+				case FieldDescriptor::CPPTYPE_UINT64:
+				{
+					break;
+				}
+				case FieldDescriptor::CPPTYPE_STRING:
+				{
+					break;
+				}
+				case FieldDescriptor::CPPTYPE_FLOAT:
+				{
+					break;
+				}
+				case FieldDescriptor::CPPTYPE_DOUBLE:
+				{
+					break;
+				}
+				case FieldDescriptor::CPPTYPE_MESSAGE:
+				{
+					break;
+				}
+				case FieldDescriptor::CPPTYPE_BOOL:
+				{
+					break;
+				}
+				case FieldDescriptor::CPPTYPE_ENUM:
+				{
+					break;
+				}
+			}
+		}
 	}
 
 	double d = sheet->readNum(3, 1);
@@ -144,9 +246,18 @@ bool CommLoad<key, value>::load2Map(const string& conf_file, int32_t index, map<
 
 	__END_PROC__
 
-	book->release();
+	if(book)
+	{
+		book->release();
+	}
 
-	return true;
+	if(msg)
+	{
+		msg->Clear();
+		delete msg;
+	}
+
+	return bRet;
 
 //    __TRY__
 //
