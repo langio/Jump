0edbeabdcf113f06a4de8d27f0f88df8a8d6fad0
langio <langio@github.com>
2015-09-18 19:49:19 +0800

    test

--- server/3rd/libxl-3.6.2.0/examples/c++/read_my.cpp
+++ server/3rd/libxl-3.6.2.0/examples/c++/read_my.cpp
@@ -26,6 +26,18 @@ int main()
                 int h = sheet->lastRow();
                 cout << h << endl;
 
+                cout << "firstRow:" << sheet->firstRow() << endl;
+                cout << "lastRow:" << sheet->lastRow() << endl;
+                cout << "firstCol:" << sheet->firstCol() << endl;
+                cout << "lastCol:" << sheet->lastCol() << endl;
+
+                cout << "lastUinit:" << sheet->readStr(sheet->lastRow()-1, sheet->lastCol()-1) << endl;
+
+                const char* last = sheet->readStr(sheet->lastRow(), sheet->lastCol());
+                if(NULL == last)
+                {
+                	cout << "last is empty!" << endl;
+                }
 //                int year, month, day;
 //                book->dateUnpack(sheet->readNum(4, 1), &year, &month, &day);
 //                cout << year << "-" << month << "-" << day << endl;
