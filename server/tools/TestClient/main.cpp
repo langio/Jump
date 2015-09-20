4ef79353bfb47e38e76cf08782f630a0c34fafad
langio <langio@foxmail.com>
2015-09-20 23:00:58 +0800

    test

--- server/tools/TestClient/main.cpp
+++ server/tools/TestClient/main.cpp
@@ -9,5 +9,10 @@ int main(int argc, char* argv[])
 	struct socket_server * ss = socket_server_create();
 	int32_t conn_id = socket_server_connect(ss, 0, "127.0.0.1", 8888);
 
+	char buff[1024];
+	//发送数据
+	socket_server_send(ss, conn_id, buff, 1024);
+
+	socket_server_release(ss);
 	return 0;
 }
