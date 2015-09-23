3fa73d553d8d4810a37d91ae07a7ffc64a9e7821
langio <langio@foxmail.com>
2015-09-23 22:04:03 +0800

    mk

--- server/protocol/proto.mk
+++ server/protocol/proto.mk
@@ -3,8 +3,10 @@ PROTO_DIR := $(shell dirname `echo $(MAKEFILE_LIST) | awk '{for(i=1;i<=NF;++i){i
 
 INCLUDE += -I$(PROTO_DIR)/pb -I$(PROTO_DIR)/proto
 
-PB_LIB_DIR = /usr/local/lib/
+#PB_LIB_DIR = /usr/local/lib/
+PB_LIB_DIR = /usr/lib/x86_64-linux-gnu/
 
+#SO_LIBS += -L$(PROTO_DIR) -lproto -L$(PB_LIB_DIR) -lprotobuf -Wl,-rpath,$(PB_LIB_DIR)
 LIBS += -L$(PROTO_DIR) -lproto -L$(PB_LIB_DIR) -lprotobuf -Wl,-rpath,$(PB_LIB_DIR)
 
 #LIBS += -L $(LIBPATH) -lxl -Wl,-rpath,$(LIBPATH),-rpath,../$(LIBPATH)
