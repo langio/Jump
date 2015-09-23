60192dd784b1fd482e813747454490da1aa6b2fa
langio <langio@github.com>
2015-09-23 13:05:23 +0800

    util

--- server/comm/send_util.h
+++ server/comm/send_util.h
@@ -1,8 +1,7 @@
 #include "skynet_socket.h"
-#include "pkg_head.pb.h"
 #include <google/protobuf/message.h>
 
-int sendToClinet(int socket_id, const pkg_head& head, const google::protobuf::Message& message)
+int sendToClinet(int socket_id, const google::protobuf::Message& message)
 {
 	return 0;
 }
