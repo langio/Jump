ea727143296c5a90a8d1adc7cc556597728e5eea
langio <langio@foxmail.com>
2015-10-04 22:35:55 +0800

    g

--- server/protocol/gen_pb.sh
+++ server/protocol/gen_pb.sh
@@ -2,7 +2,15 @@
 
 cd $(dirname "$0") || exit 1
 
-proto_file_dir=(proto storage)
+cur_dir=`pwd`
+
+proto_file_dir=(proto storage conf)
+
+PT=
+for dir in ${proto_file_dir[@]}
+do
+	PT=$PT" --proto_path="$dir
+done
 
 new_file_dir=pb/new
 old_file_dir=pb
@@ -17,7 +25,8 @@ function gen()
 	find $dir -maxdepth 1 -iname "*.proto" | while read proto_file;
 	do
 		base_name=`basename $proto_file .proto`
-		protoc --proto_path=$dir --cpp_out=$new_file_dir $proto_file
+		#protoc --proto_path=$dir --proto_path=$cur_dir --cpp_out=$new_file_dir $proto_file
+		protoc $PT --cpp_out=$new_file_dir $proto_file
 
 		if [ $? -ne 0 ]; then
 			echo "process $proto_file error!"
