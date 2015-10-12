6b0e255dd94c36e10b6cc34438bec374c6f28062
langio <langio@github.com>
2015-10-12 15:43:34 +0800

    make

--- server/3rd/redox/make.sh
+++ server/3rd/redox/make.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
+INSTALL_PATH=`pwd`
+echo $INSTALL_PATH
 cd ../package/redox &&
-chmod +x make.sh &&
-dos2unix make.sh &&
-./make.sh &&
+mkdir -p build
+
 cd build &&
-sudo make install
+cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH .. &&
+time make &&
+make install &&
+
+if [ -x "$INSTALL_PATH/lib64" ]; then
+	cp -v libredox_static.a $INSTALL_PATH/lib64
+fi
+
+if [ -x "$INSTALL_PATH/lib32" ]; then
+	cp -v libredox_static.a $INSTALL_PATH/lib32
+fi
+
+
+#chmod +x make.sh &&
+#dos2unix make.sh &&
+#./make.sh &&
+#cd build &&
+#sudo make install
--- server/3rd/redox/redox.mk
+++ server/3rd/redox/redox.mk
@@ -1,6 +1,7 @@
 REDOX_DIR := $(shell dirname `echo $(MAKEFILE_LIST) | awk '{for(i=1;i<=NF;++i){if(\$$i~"redox.mk") {print \$$i;exit}}}'`)
 
-#INCLUDE += -I$(REDOX_DIR)/include
-LIBS += -lredox -Wl,-rpath,/usr/local/lib64/
+INCLUDE += -I$(REDOX_DIR)/include
+#LIBS += -lredox -Wl,-rpath,/usr/local/lib64/
+LIBS += -L$(REDOX_DIR)/lib64 -lredox_static -lev -lhiredis
 
 
