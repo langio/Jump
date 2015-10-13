a9f58f0271d10da1a66f3005bd0f620fa58123dd
langio <langio@github.com>
2015-10-13 20:17:45 +0800

    mgr

---
+++ server/zone/GameServer/src/player_data_mgr.cpp
@@ -0,0 +1,1 @@
+#include "player_data_mgr.h"
---
+++ server/zone/GameServer/src/player_data_mgr.h
@@ -0,0 +1,43 @@
+#ifndef __PLAYER_DATA_MGR_H_
+#define __PLAYER_DATA_MGR_H_
+
+#include "comm_util.h"
+#include "hash_mem_pool.h"
+
+class PlayerDataMgr
+{
+    typedef HashMemPool<uint64_t, PProfile> PlayerDataPool;
+
+public:
+
+    static PlayerDataMgr& getInstance()
+	{
+		static PlayerDataMgr _instance;
+		return _instance;
+	}
+
+    size_t GetMaxSize(unsigned int bucket, unsigned int node);
+
+    int Init(void* mem, uint32_t bucket_num, uint32_t max_node, bool check_header = false);
+
+    PProfile * Add(PlayerID player_id, bool & exist);
+
+    /*
+     * 游戏上层逻辑取在线玩家数据，保证数据已经加载,并且玩家在线
+     */
+    PProfile *GetPlayerData(PlayerID player_id);
+
+    int Del(PlayerID player_id);
+
+
+private:
+    PlayerDataMgr(){}
+    PlayerDataMgr(const PlayerDataMgr&);
+    PlayerDataMgr& operator=(const PlayerDataMgr&);
+
+
+    PlayerDataPool _player_data_pool;
+};
+
+
+#endif
